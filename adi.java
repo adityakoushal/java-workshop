4183f17a005cd89bd6d9cd9c921654ee485995ee
adityakoushal <aaditykoushal1@gmail.com>
2026-06-08 17:15:18 +0530

    First Java Programs

---
+++ adi.java
@@ -0,0 +1,9 @@
+public class adi{
+ public static void main(String args[]){
+System.out.println("Hello world");
+int a=10 , b=20 ;
+
+int c=a+b;
+System.out.println("sum is "+c);
+}
+}
---
+++ integerliterlas.java
@@ -0,0 +1,12 @@
+public class integerliterlas{
+    public static void main(String args[]){
+        int a = 104;
+        int b = 0150;     
+        int c = 0b1101000; 
+        int d = 0x68;      
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
