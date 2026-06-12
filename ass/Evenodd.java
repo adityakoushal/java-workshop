e99fcf796dbd3a795885c969eed0e8fa2c360a16
adityakoushal <aaditykoushal1@gmail.com>
2026-06-12 08:05:31 +0530

    patternprograms

--- ass/Evenodd.java
+++ ass/Evenodd.java
@@ -1,15 +1,23 @@
-import java.util.Scanner;
-
 public class EvenOdd {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
+	public static void main(String[] args) {
+		java.util.Scanner sc=new java.util.Scanner(System.in);
+		
+		System.out.println("Enter a number:");
+		int a=sc.nextInt();
+		
+		if(a%2==0) {
+			System.out.println(a+" is Even");
+		}else {
+			System.out.println(a+" is ODD");
+		}
+		
+		System.out.println("Thanks for using APP");
+		
+		sc.close();
+	}
 
-        String result = (n % 2 == 0) ? "Even" : "Odd";
+}
+public class EvenOdd {
 
-        System.out.println(result);
-        sc.close();
-    }
 }
